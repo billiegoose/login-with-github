e687cf5a9d452ad61bc5df84cd2785ed68c94541
Will Hilton <wmhilton@gmail.com>
2016-11-06 13:02:57 -0500

    Confusion

--- login-with-github-vue-component/index.js
+++ login-with-github-vue-component/index.js
@@ -1,16 +1,8 @@
-// Borrowing heavily from https://medium.com/@heatherbooker/how-to-publish-an-vue-js-directive-to-npm-e98600fb5d2f#.xssbsywp4
-try {
-  let Vue = require('vue')
-} catch (e) {
-  // It's all good, Vue should already be available globally.
-}
-
-let LoginWithGithubButton = require('./login-with-github-button.vue')
-
-// Finally, check if you should export it:
-try {
-  module.exports = LoginWithGithubButton
-} catch (e) {
-  // Can't export it, so assume in a script and should install it globally.
-  Vue.component('login-with-github', LoginWithGithubButton)
+"use strict"
+const LoginWithGithubButton = require('./src/component.vue')
+module.exports = {
+  'LoginWithGithub': LoginWithGithubButton,
+  install: function(Vue) {
+    Vue.component('login-with-github', LoginWithGithubButton)
+  }
 }
