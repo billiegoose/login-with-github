9d8bca064c311f9b8e45ee96abb8c4bcaf3bac0a
Will Hilton <wmhilton@gmail.com>
2016-11-08 23:26:48 -0500

    Fix 3 different problems with route-handler

--- login-with-github-route-handler/route.js
+++ login-with-github-route-handler/route.js
@@ -1,7 +1,7 @@
 "use strict"
 const request = require('superagent')
 
-const error_template = (msg) => `<html><body><pre>msg</pre></body></html>`
+const error_template = (msg) => `<html><body><pre>${msg}</pre></body></html>`
 
 const success_template = (body) => `<html>
 <body>
@@ -13,16 +13,25 @@ const success_template = (body) => `<html>
 </html>`
 
 const routeHandler = (options) => {
+  if (options.client_id === null || typeof options.client_id === "undefined") {
+    throw Error("OAuth 'client_id' is a required option")
+  }
+  if (options.client_secret === null || typeof options.client_secret === "undefined") {
+    throw Error("OAuth 'client_secret' is a required option")
+  }
   return (req, client_res) => {
     request.post(options.authorization_url)
     .send({
       client_id: options.client_id,
       client_secret: options.client_secret,
-      code: req.params.code,
-      state: req.params.state
+      code: req.query.code,
+      state: req.query.state
     })
     .accept('json')
-    .end((github_res) => {
+    .end((err, github_res) => {
+      if (err) {
+        return client_res.status(500).type('html').send(error_template(`Internal Error: ${err.message}`))
+      }
       if (!github_res.ok) {
         return client_res.status(500).type('html').send(error_template(`Error ${github_res.status}: ${github_res.statusText}`))
       }
@@ -30,7 +39,7 @@ const routeHandler = (options) => {
         return client_res.status(400).type('html').send(error_template(`Error 400: ${JSON.stringify(github_res.body, null, 2)}`))
       }
       return client_res.status(200).type('html').send(success_template({
-        state: req.params.state,
+        state: req.query.state,
         access_token: github_res.body.access_token
       }))
     })
