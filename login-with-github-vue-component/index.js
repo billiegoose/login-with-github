6459cb75c6a6894eb1aa095009284ae39e9b4044
Will Hilton <wmhilton@gmail.com>
2016-11-08 23:32:58 -0500

    Delete unnecessary files

--- login-with-github-vue-component/index.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-const LoginWithGithubButton = require('./src/component.vue')
-module.exports = {
-  'LoginWithGithub': LoginWithGithubButton,
-  install: function(Vue) {
-    Vue.component('login-with-github', LoginWithGithubButton)
-  }
-}
